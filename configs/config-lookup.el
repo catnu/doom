04f362f1c2239c10bbb3afcc041984685399a497
catnu <catnu.cs@gmail.com>
2023-08-10 11:55:30 +0800

    update lookup
    - better interactive with brief buf

--- configs/config-lookup.el
+++ configs/config-lookup.el
@@ -74,56 +74,108 @@ QUERY must be a string, and PROVIDER must be a key of
   ;;       translate-shell-brief-command "proxychains4 -q trans -brief -t zh+en %s"))
 
 ;; Dictionary
-(defvar config-lookup/brief--buffer " *explanation-brief-buffer*")
-(defvar config-lookup/brief-buffer--show nil)
+(defvar config-lookup/brief--buffer-alist '(" *explanation-brief-buffer*" " *habitica-brief-buffer"))
+(defvar config-lookup/brief--buffer-show nil)
 
-(defun config-lookup/explanation-brief-pop ()
+;; (defun config/explation-buf ()
+;;   (car config-lookup/brief--buffer-alist))
+
+;; (defun config/habitica-buf ()
+;;   (cadr config-lookup/brief--buffer-alist))
+
+(defun config-lookup/brief-pop (buf-or-name)
   (when (posframe-workable-p)
-    (setq config-lookup/brief-buffer--show t)
-    (posframe-show config-lookup/brief--buffer
+    (and config-lookup/brief--buffer-show
+         (posframe-hide config-lookup/brief--buffer-show))
+    (setq config-lookup/brief--buffer-show buf-or-name)
+    (posframe-show buf-or-name
                    ;; :poshandler 'posframe-poshandler-frame-top-right-corner
                    :poshandler #'(lambda (_info) '(-25 . 25))
                    ;; :background-color "green"
                    :foreground-color "orange"
                    :border-width 1
                    :border-color "orange"
+                   :max-height 37
+                   :max-width 50
                    :left-fringe 18
                    :right-fringe 18)))
 
-(defun config-lookup/brief-buffer-pop-toggle () (interactive)
-       (cond (config-lookup/brief-buffer--show
-              (setq config-lookup/brief-buffer--show nil)
-              (posframe-hide config-lookup/brief--buffer))
-             (t (config-lookup/explanation-brief-pop))))
+(defun config-lookup/brief-hide ()
+  (posframe-hide config-lookup/brief--buffer-show)
+  (setq config-lookup/brief--buffer-show nil))
+
+(defun config-lookup/brief-buffer-pop-toggle ()
+  (interactive)
+  (pcase-let ((`(,explanation-buf ,habitica-buf) config-lookup/brief--buffer-alist))
+    (cond
+     ((null config-lookup/brief--buffer-show)
+      (config-lookup/brief-pop
+       (if (string= "Habitica" (completing-read "Show Which: " '("Translation" "Habitica" )))
+           habitica-buf explanation-buf)))
+     ((string= explanation-buf config-lookup/brief--buffer-show)
+      (if (string= "Habitica" (completing-read "Show Which: " '("Close" "Habitica")))
+          (config-lookup/brief-pop explanation-buf)
+        (config-lookup/brief-hide)))
+     ((string= habitica-buf config-lookup/brief--buffer-show)
+      (let ((select (completing-read "Show Which: " '("Close" "Translation" "Habitica Profile" ))))
+        (cond
+         ((string= "Close" select) (config-lookup/brief-hide))
+         ((string= "Translation" select) (config-lookup/brief-pop explanation-buf))
+         (t (config-lookup/profile-button-pressed nil))))))))
+
+(defun config-lookup/translate-shell-mix (word)
+  (let ((result
+         (concat
+          (replace-regexp-in-string "\n" "; "
+                                    (shell-command-to-string
+                                     (format translate-shell-brief-command
+                                             (shell-quote-argument word))))
+          "\n\n" (shell-command-to-string
+                (format translate-shell-command (shell-quote-argument word))))))
+    (add-to-list 'translate-shell-brief-cache (cons (intern word) result))
+    result))
 
 (defun config-lookup/explanation-brief (&optional word)
   "Show the explanation of WORD in the echo area."
   (interactive
    (list (translate-shell--read-string)))
   (message "google translating...")
-  (let* ((word-sym (intern word))
+  (let* ((buf (car config-lookup/brief--buffer-alist))
+         (word-sym (intern word))
          (result (if (assq word-sym translate-shell-brief-cache)
                      (assoc-default word-sym translate-shell-brief-cache)
-                   (shell-command-to-string
-                    (format translate-shell-brief-command (shell-quote-argument word))))))
+                   (config-lookup/translate-shell-mix word))))
     (message "google translated")
-    (with-current-buffer (get-buffer-create config-lookup/brief--buffer)
+    (with-current-buffer (get-buffer-create buf)
       (erase-buffer)
       (insert "\n")
       (insert result)
       (insert "\n"))
-    (config-lookup/explanation-brief-pop)))
+    (config-lookup/brief-pop buf)))
+
+(defun config-lookup/profile-button-pressed (_button)
+  ;; (funcall +lookup-open-url-fn private/habitica-profile-url)
+  (funcall +lookup-open-url-fn "https://habitica.com/"))
+
+(define-button-type 'config-lookup/profile-button
+  'action 'config-lookup/profile-button-pressed
+  'follow-link t
+  'help-echo "Click Button"
+  'help-args "test")
 
 (defun config-lookup/brief-buffer-habitica-character ()
   (interactive)
   (if (or (not habitica-uid) (not habitica-token))
       (call-interactively 'habitica-login))
-  (with-current-buffer (get-buffer-create config-lookup/brief--buffer)
-    (erase-buffer)
-    (insert "\n")
-    (habitica--parse-profile (assoc-default 'stats (habitica-api-get-profile)) nil)
-    (insert "\n"))
-  (config-lookup/explanation-brief-pop))
+  (let ((buf (cadr config-lookup/brief--buffer-alist)))
+    (with-current-buffer (get-buffer-create buf)
+      (erase-buffer)
+      (insert "\n")
+      (habitica--parse-profile (assoc-default 'stats (habitica-api-get-profile)) nil)
+      (insert "** Profile")
+      (make-button (- (point) 10) (point) :type 'config-lookup/profile-button)
+      (insert "\n\n"))
+    (config-lookup/brief-pop buf)))
 
 (map! :leader
       ;; :desc "Translation" "st" #'config-lookup/explanation-brief
