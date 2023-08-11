e7b2aa349f5f30c0482bb9256281f7d2f83b7a29
catnu <catnu.cs@gmail.com>
2023-08-11 23:32:30 +0800

    add minibrief

--- configs/config-lookup.el
+++ configs/config-lookup.el
@@ -1,5 +1,8 @@
 ;;; configs/config-lookup.el -*- lexical-binding: t; -*-
 
+;; use translation habitica with minibrief
+(require 'config-minibrief)
+
 ;;; more query
 (add-to-list '+lookup-provider-url-alist
              '("Emacs China" "https://emacs-china.org/search?expanded=true&q=%s"))
@@ -16,7 +19,7 @@
 (defun config-lookup/url-action (url)
   (pcase url
     ((rx "lookup://:translate:/" (let query (0+ anything)))
-     (funcall-interactively #'config-lookup/explanation-brief
+     (funcall-interactively #'minibrief/shwo-translation
                             (if (eq query "")
                                 (or (thing-at-point 'word)
                                     (translate-shell--read-string))
@@ -84,124 +87,10 @@ QUERY must be a string, and PROVIDER must be a key of
   (advice-remove '+lookup/online-select #'config-lookup/use-search-online))
 (advice-add '+lookup/online-select :before #'config-lookup/use-search-online)
 
-;; need https://github.com/soimort/translate-shell
-(use-package translate-shell)
-  ;; :config
-  ;; <https://translate.google.com> is blocked in China for no apparent
-  ;; reason. No one ever asked my option.
-  ;; for mac you need proxychains-ng installed
-  ;; (setq translate-shell-command "proxychains4 -q trans -t en+zh %s"
-  ;;       translate-shell-brief-command "proxychains4 -q trans -brief -t zh+en %s"))
-
-;; Dictionary
-(defvar config-lookup/brief--buffer-alist '(" *explanation-brief-buffer*" " *habitica-brief-buffer"))
-(defvar config-lookup/brief--buffer-show nil)
-
-;; (defun config/explation-buf ()
-;;   (car config-lookup/brief--buffer-alist))
-
-;; (defun config/habitica-buf ()
-;;   (cadr config-lookup/brief--buffer-alist))
-
-(defun config-lookup/brief-pop (buf-or-name)
-  (when (posframe-workable-p)
-    (and config-lookup/brief--buffer-show
-         (posframe-hide config-lookup/brief--buffer-show))
-    (setq config-lookup/brief--buffer-show buf-or-name)
-    (posframe-show buf-or-name
-                   ;; :poshandler 'posframe-poshandler-frame-top-right-corner
-                   :poshandler #'(lambda (_info) '(-25 . 25))
-                   ;; :background-color "green"
-                   :foreground-color "orange"
-                   :border-width 1
-                   :border-color "orange"
-                   :max-height 37
-                   :max-width 50
-                   :left-fringe 18
-                   :right-fringe 18)))
-
-(defun config-lookup/brief-hide ()
-  (posframe-hide config-lookup/brief--buffer-show)
-  (setq config-lookup/brief--buffer-show nil))
-
-(defun config-lookup/brief-buffer-pop-toggle ()
-  (interactive)
-  (pcase-let ((`(,explanation-buf ,habitica-buf) config-lookup/brief--buffer-alist))
-    (cond
-     ((null config-lookup/brief--buffer-show)
-      (config-lookup/brief-pop
-       (if (string= "Habitica" (completing-read "Show Which: " '("Translation" "Habitica" )))
-           habitica-buf
-         explanation-buf)))
-     ((string= explanation-buf config-lookup/brief--buffer-show)
-      (if (string= "Habitica" (completing-read "Show Which: " '("Close" "Habitica")))
-          (config-lookup/brief-pop habitica-buf)
-        (config-lookup/brief-hide)))
-     ((string= habitica-buf config-lookup/brief--buffer-show)
-      (let ((select (completing-read "Show Which: " '("Close" "Translation" "Habitica Profile" ))))
-        (cond
-         ((string= "Close" select) (config-lookup/brief-hide))
-         ((string= "Translation" select) (config-lookup/brief-pop explanation-buf))
-         (t (config-lookup/profile-button-pressed nil))))))))
-
-(defun config-lookup/translate-shell-mix (word)
-  (let ((result
-         (concat
-          (replace-regexp-in-string "\n" "; "
-                                    (shell-command-to-string
-                                     (format translate-shell-brief-command
-                                             (shell-quote-argument word))))
-          "\n\n" (shell-command-to-string
-                (format translate-shell-command (shell-quote-argument word))))))
-    (add-to-list 'translate-shell-brief-cache (cons (intern word) result))
-    result))
-
-(defun config-lookup/explanation-brief (&optional word)
-  "Show the explanation of WORD in the echo area."
-  (interactive
-   (list (translate-shell--read-string)))
-  (message "google translating...")
-  (let* ((buf (car config-lookup/brief--buffer-alist))
-         (word-sym (intern word))
-         (result (if (assq word-sym translate-shell-brief-cache)
-                     (assoc-default word-sym translate-shell-brief-cache)
-                   (config-lookup/translate-shell-mix word))))
-    (message "google translated")
-    (with-current-buffer (get-buffer-create buf)
-      (erase-buffer)
-      (insert "\n")
-      (insert result)
-      (insert "\n"))
-    (config-lookup/brief-pop buf)))
-
-(defun config-lookup/profile-button-pressed (_button)
-  ;; (funcall +lookup-open-url-fn private/habitica-profile-url)
-  (funcall +lookup-open-url-fn "https://habitica.com/"))
-
-(define-button-type 'config-lookup/profile-button
-  'action 'config-lookup/profile-button-pressed
-  'follow-link t
-  'help-echo "Click Button"
-  'help-args "test")
-
-(defun config-lookup/brief-buffer-habitica-character ()
-  (interactive)
-  (if (or (not habitica-uid) (not habitica-token))
-      (call-interactively 'habitica-login))
-  (let ((buf (cadr config-lookup/brief--buffer-alist)))
-    (with-current-buffer (get-buffer-create buf)
-      (erase-buffer)
-      (insert "\n")
-      (habitica--parse-profile (assoc-default 'stats (habitica-api-get-profile)) nil)
-      (insert "** Profile")
-      (make-button (- (point) 10) (point) :type 'config-lookup/profile-button)
-      (insert "\n\n"))
-    (config-lookup/brief-pop buf)))
-
 (map! :leader
-      ;; :desc "Translation" "st" #'config-lookup/explanation-brief
-      :desc "Bref buffer Helper" "hh" #'config-lookup/brief-buffer-pop-toggle
-      :desc "Bref buffer Habitica" "hH" #'config-lookup/brief-buffer-habitica-character
+      ;; :desc "Translation" "st" #'minibrief/shwo-translation
+      :desc "Bref buffer Helper" "hh" #'minibrief/pop-toggle
+      :desc "Bref buffer Habitica" "hH" #'minibrief/show-habitica-character
       :desc "Look up online (w/ prompt)" "so" #'+lookup/online-select
       "sO" nil)
 
---
+++ configs/config-minibrief.el
@@ -0,0 +1,120 @@
+;;; configs/config-minibrief.el -*- lexical-binding: t; -*-
+;;; minibrief = posframe minibuffer at right top
+
+(defvar minibrief/name-list '("Habitica" "Translation" "*Messages*"))
+(defvar minibrief/show--name nil)
+
+(defun minibrief/buffer-name (name)
+  (if (string-match-p "^*" name) name (format " *Minibrief %s*" name)))
+
+(defun minibrief/options (&optional list)
+  (append (or list (remove minibrief/show--name minibrief/name-list)) '("Close")))
+
+(defun minibrief/show (brief-name &optional color height width)
+  (and minibrief/show--name (minibrief/hide minibrief/show--name))
+  (setq minibrief/show--name brief-name)
+  (posframe-show (minibrief/buffer-name brief-name)
+                 ;; :poshandler 'posframe-poshandler-frame-top-right-corner
+                 :poshandler #'(lambda (_info) '(-25 . 25))
+                 ;; :background-color "green"
+                 :foreground-color (or color "orange")
+                 :border-width 1
+                 :border-color (or color "orange")
+                 :height height
+                 :width width
+                 :max-height 37
+                 :max-width 50
+                 :left-fringe 18
+                 :right-fringe 18))
+
+(defun minibrief/hide (brief-name &optional delete)
+  ;; (when (string= brief-name "*Messages*")
+  ;;   (advice-remove 'message #'minibrief/message-tail)
+  ;;   (setq delete t))
+  (funcall (if delete #'posframe-delete #'posframe-hide) (minibrief/buffer-name brief-name)))
+
+(defun minibrief/message-tail (&rest _args)
+  ;; ;; (with-current-buffer "*Messages*" (goto-char (point-max)))
+  (let* ((windows (get-buffer-window-list "*Messages*" t t))
+         (window (and windows (car windows))))
+    (and window (with-current-buffer (window-buffer window)
+                  (set-window-point window (point-max))))))
+
+(defun minibrief/habitica-profile-button-pressed (_button)
+  ;; (funcall +lookup-open-url-fn private/habitica-profile-url)
+  (funcall +lookup-open-url-fn "https://habitica.com/"))
+
+(define-button-type 'minibrief/profile-button
+  'action 'minibrief/habitica-profile-button-pressed
+  'follow-link t
+  'help-echo "Click Button"
+  'help-args "test")
+
+(defun minibrief/show-habitica-character ()
+  (interactive)
+  (if (or (not habitica-uid) (not habitica-token))
+      (call-interactively 'habitica-login))
+  (let ((buf (minibrief/buffer-name "Habitica")))
+    (with-current-buffer (get-buffer-create buf)
+      (erase-buffer)
+      (insert "\n")
+      (habitica--parse-profile (assoc-default 'stats (habitica-api-get-profile)) nil)
+      (insert "** Profile")
+      (make-button (- (point) 10) (point) :type 'minibrief/profile-button)
+      (insert "\n\n"))
+    (minibrief/show "Habitica")))
+
+;; need https://github.com/soimort/translate-shell
+(use-package translate-shell)
+  ;; :config
+  ;; <https://translate.google.com> is blocked in China for no apparent
+  ;; reason. No one ever asked my option.
+  ;; for mac you need proxychains-ng installed
+  ;; (setq translate-shell-command "proxychains4 -q trans -t en+zh %s"
+  ;;       translate-shell-brief-command "proxychains4 -q trans -brief -t zh+en %s"))
+
+(defun minibrief/translate-shell-mix (word)
+  (let ((result
+         (concat
+          (replace-regexp-in-string "\n" "; "
+                                    (shell-command-to-string
+                                     (format translate-shell-brief-command
+                                             (shell-quote-argument word))))
+          "\n\n" (shell-command-to-string
+                (format translate-shell-command (shell-quote-argument word))))))
+    (add-to-list 'translate-shell-brief-cache (cons (intern word) result))
+    result))
+
+(defun minibrief/shwo-translation (&optional word)
+  "Show the explanation of WORD in the echo area."
+  (interactive
+   (list (translate-shell--read-string)))
+  (message "google translating...")
+  (let* ((buf (minibrief/buffer-name "Translation"))
+         (word-sym (intern word))
+         (result (if (assq word-sym translate-shell-brief-cache)
+                     (assoc-default word-sym translate-shell-brief-cache)
+                   (minibrief/translate-shell-mix word))))
+    (message "google translated")
+    (with-current-buffer (get-buffer-create buf)
+      (erase-buffer)
+      (insert "\n")
+      (insert result)
+      (insert "\n"))
+    (minibrief/show "Translation")))
+
+
+(defun minibrief/pop-toggle ()
+  (interactive)
+  (let ((select (completing-read "Show: " (minibrief/options))))
+  (pcase select
+    ("Close" (minibrief/hide minibrief/show--name)
+     (setq minibrief/show--name nil))
+    ("*Messages*" (minibrief/show select "#bbc2cf" 37 50)
+     (minibrief/message-tail)
+     (advice-add 'message :after #'minibrief/message-tail))
+    ((guard (member select minibrief/name-list)) (minibrief/show select)); todo memq
+    (unknown (message (format "unknown minibrief name '%s'" unknown))))))
+
+(message "[config] Apply config-minibrief")
+(provide 'config-minibrief)
